efa67f57d3d3aa3b9e011181acbf8dc85133294a
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-10-18 02:25:37 +0800

    Created design prototype

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -5,12 +5,18 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding .inflate(getLayoutInflater());
         setContentView(R.layout.activity_main)
 
+        /*
         val display: TextView = findViewById(R.id.text_display)
         val button: Button = findViewById(R.id.button)
         val input: EditText = findViewById(R.id.text_input)
@@ -19,5 +25,6 @@ class MainActivity : AppCompatActivity() {
             display.text = input.text
             input.setText("")
         }
+         */
     }
 }
