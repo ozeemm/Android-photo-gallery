54e91bb388e49308a3227285610f62d79fca50e5
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-11-01 11:18:43 +0800

    Lab3 Done (See branch Lab3)

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.gallery
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        val inputAlbumName = findViewById<EditText>(R.id.inputAlbumName)
+        val galleryLinearLayout = findViewById<LinearLayout>(R.id.galleryLinearLayout)
+
+        val photoList = ArrayList<Photo>()
+
+        addButton.setOnClickListener{
+            val album = inputAlbumName.text.toString()
+
+            val photo = Photo(album)
+            photoList.add(photo)
+
+            inputAlbumName.text.clear()
+
+            val photoItem = getNewPhotoItem(photo, galleryLinearLayout)
+            galleryLinearLayout.addView(photoItem)
         }
+    }
+
+    private fun getNewPhotoItem(photo: Photo, galleryLinearLayout: LinearLayout): View {
+        val photoItem = layoutInflater.inflate(R.layout.photo_item, galleryLinearLayout, false)
+
+        val photoImage = photoItem.findViewById<ImageView>(R.id.photoImage)
+        val photoAlbumName = photoItem.findViewById<TextView>(R.id.photoAlbumName)
+        val photoDate = photoItem.findViewById<TextView>(R.id.photoDate)
+
+        photoAlbumName.text = photo.album
+        photoDate.text = photo.date
+
+        return photoItem
     }
 }
