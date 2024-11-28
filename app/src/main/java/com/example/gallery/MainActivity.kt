06187f9beef19149cbfc1bbdd31d4db1ae50f97e
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-11-29 00:57:17 +0800

    Added data storage in SharedPreference

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sharedPreferences = SharedPreferenceWorker(baseContext)
+        sharedPreferences = SharedPreferenceWorker(this)
         gestureDetector = GestureDetector(this, SwipeListener())
 
         createPhotoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
